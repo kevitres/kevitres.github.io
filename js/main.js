4f30a125e3c8cf42b5b691de08312483d1cd4414
kevitres <kevin.tresinie@student.arteveldehs.be>
2017-08-08 16:11:12 +0200

    keuzemenu update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+
     
 $('#running').click(function(){    
 document.location.href = "../running/index.html";
@@ -16,8 +17,52 @@ $('#king').click(function(){
 document.location.href = "../achievements/index.html";
 });
     
+    $(".keuzeRunning, .keuzeSwimming, .keuzeAchievements").hide();
+    
+    
+     $("#btnPooping").click(function(){
+        $(".keuzePooping").show();
+        $(".keuzeRunning").hide();        
+        $(".keuzeSwimming").hide();
+        $(".keuzeAchievements").hide();
+    });   
+    
+    $("#btnRunning").click(function(){
+       
+        $(".keuzePooping").hide();
+        $(".keuzeRunning").show();        
+        $(".keuzeSwimming").hide();
+        $(".keuzeAchievements").hide();    
+        
+        
+        $('html').css({background : 'url("../img/background_running.jpg")'});
+        
+    });   
+    
+    $("#btnSwimming").click(function(){
+        $(".keuzePooping").hide();
+        $(".keuzeRunning").hide();        
+        $(".keuzeSwimming").show();
+        $(".keuzeAchievements").hide();
+    });
+    
+     $("#btnAchievements").click(function(){
+        $(".keuzePooping").hide();
+        $(".keuzeRunning").hide();        
+        $(".keuzeSwimming").hide();
+        $(".keuzeAchievements").show();
+    });   
 
-}
+
+
+};
 $(document).ready(function () {
-    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav(); 
+
+     
 });
+
+
+
+
+
